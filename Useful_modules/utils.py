0392d6b14170332067ee065357ff916002bdccbc
Chengyu Huang <huangchengyu16@gmail.com>
2020-05-31 22:05:38 -0400

    Create utils.py

---
+++ Useful_modules/utils.py
@@ -0,0 +1,9 @@
+from collections import Iterable
+
+## flaten nested lists
+def flatten(l):
+    for el in l:
+        if isinstance(el, Iterable) and not isinstance(el, (str, bytes)):
+            yield from flatten(el)
+        else:
+            yield el
